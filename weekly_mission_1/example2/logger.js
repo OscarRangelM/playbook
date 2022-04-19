c90bf45da99978b9386e4847deb8dc737713a694
OscarRangelM <orangelm1300@alumno.ipn.mx>
2022-04-18 20:51:39 -0500

    Cremos los archivos logger y main para conectarlos

---
+++ weekly_mission_1/example2/logger.js
@@ -0,0 +1,9 @@
+// creamos una función para guardar el modulo info
+exports.info = (message) => {
+    console.log(`info: ${message}`)
+}
+
+// creamos una nueva función para guardar el modulo verbose
+exports.verbose = (message) => {
+    console.log(`verbose: ${message}`)
+}
---
+++ weekly_mission_1/example2/main.js
@@ -0,0 +1,4 @@
+const logger = require('./logger') //variable con un requiere que manda la info a los modulos
+
+logger.info('Este es un mensaje')
+logger.verbose('This is a verbose message')
