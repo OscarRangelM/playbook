db62a400e6556627acfd23b8589b27a91aefc330
OscarRangelM <orangelm1300@alumno.ipn.mx>
2022-04-19 10:40:11 -0500

    Creamos el ejemplo 10

---
+++ weekly_mission_1/example10/pokemon.js
@@ -0,0 +1,15 @@
+export default class MyPokemon {
+    constructor(name){
+        this.name = name
+    }
+
+    sayHello() {
+        console.log(' Mi pokemon ' + this.name + ' te saluda!! ')
+    }
+  
+}
+
+//funcion sayHello()
+//funcion sayMessage()
+//exporta la clase para main.js
+//Mi pokemon pikachu te saluda!!!
